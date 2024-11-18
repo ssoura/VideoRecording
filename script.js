c4661174a66603a30be274b6f2a879f377b6f173
gurudeep88 <gurudeep88@gmail.com>
2024-11-18 15:24:30 +0530

    streaming bug resolved

--- script.js
+++ script.js
@@ -9,108 +9,139 @@ const stopRecordingButton = document.getElementById('stop-recording');
 SariskaMediaTransport.initialize();
 
 const GENERATE_TOKEN_URL = `https://api.sariska.io/api/v1/misc/generate-token`;
-const APP_API_KEY = {key};
+const APP_API_KEY = 'your api key';
 
-async function main() {
-  async function getToken(id, email, name) {
-    const body = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+async function getToken(id, email, name) {
+  const body = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      apiKey: APP_API_KEY,
+      user: {
+        id: id,
+        name: name,
+        email: email,
+        // moderator: name === 'admin' ? true : false
       },
-      body: JSON.stringify({
-        apiKey: APP_API_KEY,
-        user: {
-          id: id,
-          name: name,
-          email: email,
-          // moderator: name === 'admin' ? true : false
-        },
-        exp: "48 hours",
-      }),
-    };
+      exp: "48 hours",
+    }),
+  };
 
-    try {
-      const response = await fetch(GENERATE_TOKEN_URL, body);
-      if (response.ok) {
-        const json = await response.json();
-        localStorage.setItem("SARISKA_TOKEN", json.token);
-        return json.token;
-      } else {
-        console.log(response.status);
-      }
-    } catch (error) {
-      console.log("error", error);
+  try {
+    const response = await fetch(GENERATE_TOKEN_URL, body);
+    if (response.ok) {
+      const json = await response.json();
+      localStorage.setItem("SARISKA_TOKEN", json.token);
+      return json.token;
+    } else {
+      console.log(response.status);
     }
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+const createConnection = async(token) => {
+  const connection = new SariskaMediaTransport.JitsiConnection(
+    token,
+    "teststream",
+    false
+  );
+console.log('ot est conn')
+  connection.addEventListener(
+    SariskaMediaTransport.events.connection.CONNECTION_ESTABLISHED,
+    async() => {
+      console.log("connection successful!!!");
+      await createConference(connection);
+    }
+  );
+
+  // Handle connection events
+  connection.addEventListener(
+    SariskaMediaTransport.events.connection.CONNECTION_FAILED,
+    (error) => {
+      // Token expired, set again
+      if (
+        error === SariskaMediaTransport.events.connection.PASSWORD_REQUIRED
+      ) {
+        // Set a new token
+        connection.setToken(token);
+        console.log("connection disconnect!!!", error);
+      }
+    }
+  );
+
+  connection.addEventListener(
+    SariskaMediaTransport.events.connection.CONNECTION_DISCONNECTED,
+    (error) => {
+      console.log("connection disconnect!!!", error);
+    }
+  );
+
+  connection.connect();
+}
+
+const createLocalTracks = async() => {
+  
+  const resolution = 240;
+    
+  let tracks = [];
+
+  try  {
+      const [audioTrack] = await SariskaMediaTransport.createLocalTracks({devices: ["audio"], resolution});
+      tracks.push(audioTrack);
+  } catch(e) {
+      console.log("failed to fetch audio device");
   }
 
+  try  {
+      const [videoTrack]  = await SariskaMediaTransport.createLocalTracks({devices: ["video"], resolution});
+      tracks.push(videoTrack);
+  } catch(e) {
+      console.log("failed to fetch video device");
+  }
+  return tracks;
+}
+
+const createConference = async(connection) => {
+
+  const conference = connection.initJitsiConference();
+  console.log("connection",connection)
+  let localTracks = await createLocalTracks();
+
+  localTracks.forEach(async track => await conference.addTrack(track));
+
+
+  // Access local media tracks
+  const audioTrack = localTracks.find((track) => track.getType() === "audio");
+  const videoTrack = localTracks.find((track) => track.getType() === "video");
+console.log('audioTrack', audioTrack, videoTrack)
+  // Play video
+  videoTrack.attach(videoElement);
+
+  // Play audio
+  audioTrack.attach(audioElement);
+  conference.join();
+}
+
+async function main() {
+  
   const tokenRes = await getToken('ytyVgh', 'sourabh13689@gmail.com', 'sourabh');
   const token = tokenRes;
   console.log("token", token);
 
   // document.addEventListener("load", () => {
     console.log("working..")
-    const connection = new SariskaMediaTransport.JitsiConnection(
-      token,
-      "teststream",
-      true
-    );
+    await createConnection(token);
+    
 
-    connection.addEventListener(
-      SariskaMediaTransport.events.connection.CONNECTION_ESTABLISHED,
-      () => {
-        console.log("connection successful!!!");
-      }
-    );
-
-    // Handle connection events
-    connection.addEventListener(
-      SariskaMediaTransport.events.connection.CONNECTION_FAILED,
-      (error) => {
-        // Token expired, set again
-        if (
-          error === SariskaMediaTransport.events.connection.PASSWORD_REQUIRED
-        ) {
-          // Set a new token
-          connection.setToken(token);
-          console.log("connection disconnect!!!", error);
-        }
-      }
-    );
-
-    connection.addEventListener(
-      SariskaMediaTransport.events.connection.CONNECTION_DISCONNECTED,
-      (error) => {
-        console.log("connection disconnect!!!", error);
-      }
-    );
-
-    connection.connect();
-    const options = {
-      devices: ["audio", "video"],
-      resolution: 240,
-    };
-
-    const conference = connection.initJitsiConference(options);
-    console.log("connection",connection)
-    conference.join();
   // });
 
 
 
-  let localTracks;
-
-  localTracks = await SariskaMediaTransport.createLocalTracks(options);
-
-  // Access local media tracks
-  const audioTrack = localTracks.find((track) => track.getType() === "audio");
-  const videoTrack = localTracks.find((track) => track.getType() === "video");
-
-  // Play video
-  videoTrack.attach(videoElement);
-
-  // Play audio
-  audioTrack.attach(audioElement);
+  
 
 
   startRecordingButton.addEventListener('click', async () => {
@@ -135,9 +166,10 @@ async function main() {
         });
 
         if (!response.ok) {
+          console.log('first', response)
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-
+        console.log('else res', response);
         const result = await response.json();
         console.log('Recording started successfully:', result);
     } catch (error) {
